265b6d28a8d25ec0fb7252d3f94dea81f8b63159
Yalçıncan Ulus <yalcincanulus@gmail.com>
2022-04-19 00:13:23 +0300

    Add logic to determine winner

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const gameBoard = (function () {
     taken[pos] = true;
     displayController.renderBoard();
     gameController.changeCurrentPlayer();
+    gameController.checkWinner();
   };
 
   domBoard.addEventListener("click", (e) => {
     if (!e.target.classList.contains("board__square")) return;
+    if (!gameController.getGameRunning()) return;
     insert(gameController.getCurrentPlayer().piece, e.target.dataset.square);
   });
 
@@ -26,29 +28,81 @@ const gameBoard = (function () {
 
 const displayController = (function () {
   const domBoard = document.querySelector(".board");
+
   const renderBoard = () => {
     gameBoard.board.forEach((el, i) => {
       domBoard.children[i].textContent = el;
     });
   };
+
   const clearBoard = () => {
     gameBoard.board.splice(0, gameBoard.board.length);
     Array.from(domBoard.children).forEach((e) => (e.textContent = ""));
   };
 
+  const highlightWinner = (positions) => {
+    if (gameController.getGameRunning()) return;
+    const winnerSquares = positions.map((pos) =>
+      domBoard.querySelector(`[data-square="${pos}"]`)
+    );
+    winnerSquares.forEach((sq) => sq.classList.add("winner"));
+  };
+
   return {
     renderBoard,
     clearBoard,
+    highlightWinner,
   };
 })();
 
+////////////////////////////
 // game controller module
 const gameController = (function () {
+  let isGameRunning = true;
+  let rounds = 9;
+
   // Player factory
   const Player = function (piece) {
     return {
       piece,
     };
+  };
+
+  const _checkThree = (pos1, pos2, pos3) => {
+    const { board } = gameBoard;
+    if (!board[pos1] || !board[pos2]) return;
+
+    return {
+      won: board[pos1] === board[pos2] && board[pos2] === board[pos3],
+      winner: board[pos1],
+      positions: [pos1, pos2, pos3],
+    };
+  };
+
+  const checkWinner = () => {
+    rounds--;
+    const positions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const results = positions
+      .map((pos) => _checkThree(pos[0], pos[1], pos[2]))
+      .filter((el) => el?.won);
+    // as soon as we have a winner, stop playing
+    if (results.length) isGameRunning = false;
+    if (rounds === 0) {
+      console.log("DRAW");
+      return;
+    }
+    displayController.highlightWinner(results[0]?.positions);
+    if (!isGameRunning) console.log(`Winner: ${results[0].winner}`);
   };
 
   // create players
@@ -62,9 +116,12 @@ const gameController = (function () {
   };
 
   const getCurrentPlayer = () => currentPlayer;
+  const getGameRunning = () => isGameRunning;
 
   return {
+    getGameRunning,
     getCurrentPlayer,
     changeCurrentPlayer,
+    checkWinner,
   };
 })();
